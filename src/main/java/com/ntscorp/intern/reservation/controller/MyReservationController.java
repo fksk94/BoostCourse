6f3a883b060ef3f49f4ab54a966dd037d1429a97
구태완 <taewan.gu@nts-corp.com>
2021-08-23 14:18:44 +0900

    feat: 🎸 [project5] reservation
    - reservations GET API

---
+++ src/main/java/com/ntscorp/intern/reservation/controller/MyReservationController.java
@@ -0,0 +1,35 @@
+package com.ntscorp.intern.reservation.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ntscorp.intern.reservation.controller.response.ReservationsResponse;
+import com.ntscorp.intern.reservation.model.Reservation;
+import com.ntscorp.intern.reservation.model.ReservationCount;
+import com.ntscorp.intern.reservation.service.ReservationService;
+
+@RestController
+@RequestMapping("/api")
+public class MyReservationController {
+	private final ReservationService reservationService;
+
+	@Autowired
+	public MyReservationController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+
+	@GetMapping("/reservations")
+	public ResponseEntity<ReservationsResponse> getProductReservation(@RequestParam
+	String reservationEmail) {
+		List<Reservation> reservations = reservationService.getAllReservationsByEmail(reservationEmail);
+		ReservationCount reservationCount = reservationService.getReservationCountByEmail(reservationEmail);
+		ReservationsResponse reservationsResponse = new ReservationsResponse(reservations, reservationCount);
+		return ResponseEntity.ok(reservationsResponse);
+	}
+}
